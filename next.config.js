3783dfc229103a22ba6f573f6e3054dc395d2eba
info@ShannonAdams <info@majesticwarrior.com>
2025-10-23 20:31:20 -0700

    fixed contractor dashboard errors

--- next.config.js
+++ next.config.js
@@ -25,6 +25,18 @@ const nextConfig = {
       };
     }
     
+    // Fix font preload issues
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/fonts/',
+          outputPath: 'static/fonts/',
+        },
+      },
+    });
+    
     return config;
   },
 };
