b7f1e1f98236fa8fd4a519c7bbd4638adc4a7e64
info@ShannonAdams <info@majesticwarrior.com>
2025-12-03 14:37:11 -0700

    updated versions of react and next.js to fix vulnerabilities

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -179,7 +179,8 @@ function useToast() {
         listeners.splice(index, 1);
       }
     };
-  }, [state]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return {
     ...state,
